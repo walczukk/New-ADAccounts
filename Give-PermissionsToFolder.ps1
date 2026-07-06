5a643bc7931d716399bf12cfd76ca8fb35c1d204
walczukk <Kacperziom01@gmail.com>
2026-07-06 15:10:02 +0200

    Update Give-PermissionsToFolder.ps1

--- Give-PermissionsToFolder.ps1
+++ Give-PermissionsToFolder.ps1
@@ -1,31 +1,20 @@
 Import-Module ActiveDirectory
 
+$HomeDrivePath
+$NewUser
+
+$a = "$($using:HomeDrivePath)\$($using:HomeDriveFormat)\$($using:NewUser)"
+$b = "$($using:NewUser)$"
+$c = "H:\HOME\$($using:HomeDriveFormat)\$($using:NewUser)"
 function Grant-PermissionsToFolder {
-    $HomeDrivePath
-    $HomeDriveChildPath
-    $NewUser
-    $NewUserCheck = Get-ADUser -Filter {Name -eq $NewUser} -SearchBase "OU=Users,DC=domain,DC=pl"
-
-    if ($NewUserCheck) {
-        $a = "$HomeDriveChildPath\$NewUser"
-        $b = "DOMAIN\$NewUser"
-        $ACL = Get-Acl $a
-        $AR = New-Object System.Security.AccessControl.FileSystemAccessRule("$b", "Modify", "ContainerInherit, ObjectInherit", "None", "Allow")
-        $ACL.SetAccessRule($AR)
-                
-        Set-Acl $a $ACL
+    Invoke-Command -ComputerName "vm01" -Credential $credentials -ScriptBlock {
+        icacls $a /t /grant $b:M
     }
-
 }
 
 function Add-SharingToFolder {
-    $ParentSuffix = [Math]::Floor($num / 100) * 100
-    $HomeDriveFormat = "K{0:D4}" -f [int]$ParentSuffix
     $credentials
-
     Invoke-Command -ComputerName "vm01" -Credential $credentials -ScriptBlock {
-        $c = "$($using:NewUser)$"
-        $d = "H:\HOME\$($using:HomeDriveFormat)\$($using:NewUser)"
-        New-SmbShare -Name "$c" -Path "$d" -ChangeAccess "Wszyscy"
+        New-SmbShare -Name "$b" -Path "$c" -ChangeAccess "Wszyscy"
     }   
 }
